0614a9fc113f1d90dd872023f7c3065f4c5730a3
hollyford <holly_ford@hotmail.com>
2021-03-17 19:12:08 +0000

    added book details page

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Books
 
 # Create your views here.
@@ -13,3 +13,15 @@ def all_books(request):
     }
 
     return render(request, 'books/books.html', context)
+
+
+def book_detail(request, book_id):
+    """ A view to show individual book details """
+
+    book = get_object_or_404(Books, pk=book_id)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
