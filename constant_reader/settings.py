b7013d9ef180a9ca63692b3b1ac93f4b1b056369
hollyford <holly_ford@hotmail.com>
2021-03-24 10:32:44 +0000

    removed secret key and set debug

--- constant_reader/settings.py
+++ constant_reader/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'owuav5z7_bi!5&j2u1y660$^+5*kaow*1u%qrdw$i6gyj*a2yr'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['constant-reader.herokuapp.com', 'loacalhost']
 
