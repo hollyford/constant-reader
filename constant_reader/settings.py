859c8e1964c0448487ee94317b7c2e26d755adbe
hollyford <holly_ford@hotmail.com>
2021-03-24 11:24:48 +0000

    corrected heroku region

--- constant_reader/settings.py
+++ constant_reader/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     #Bucket config
     AWS_STORAGE_BUCKET_NAME = 'CONSTANT-READER'
-    AWS_S3_REGION_NAME = 'Europe'
+    AWS_S3_REGION_NAME = 'eu'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = F'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
