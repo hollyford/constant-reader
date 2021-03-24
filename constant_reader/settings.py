c06cef5333492ce4ea418b5d8030f39c8873d9fa
hollyford <holly_ford@hotmail.com>
2021-03-24 11:28:20 +0000

    changed storage bucket name into lower case

--- constant_reader/settings.py
+++ constant_reader/settings.py
@@ -167,7 +167,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     #Bucket config
-    AWS_STORAGE_BUCKET_NAME = 'CONSTANT-READER'
+    AWS_STORAGE_BUCKET_NAME = 'constant-reader'
     AWS_S3_REGION_NAME = 'eu'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
