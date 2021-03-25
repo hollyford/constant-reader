e71fe80d69f1417e5b98423508bb2c7ee6d9bc42
hollyford <holly_ford@hotmail.com>
2021-03-25 17:45:06 +0000

    added checkout views and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "You haven't added any books yet!")
+        return redirect(reverse('books'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
--- constant_reader/settings.py
+++ constant_reader/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'checkout',
 
     # other
+    'crispy_forms',
     'storages',
 ]
 
@@ -62,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'constant_reader.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -79,8 +82,13 @@ TEMPLATES = [
                 # required by allauth - do not delete!
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
