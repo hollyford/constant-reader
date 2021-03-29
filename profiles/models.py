106d223d875a22e26354d9d4bef6dc48f2c1c05f
hollyford <holly_ford@hotmail.com>
2021-03-29 19:15:12 +0000

    updated profiles models

--- profiles/models.py
+++ profiles/models.py
@@ -20,6 +20,9 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
     if created:
         UserProfile.objects.create(user=instance)
     instance.userprofile.save()
