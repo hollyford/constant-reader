2bb2a5a6b47c81bc32ba5faf8f908e0640e891ec
hollyford <holly_ford@hotmail.com>
2021-03-29 19:45:36 +0000

    updated imports in views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import login_required
-
 from .models import UserProfile
-from books.models import Books
 from .forms import UserProfileForm
 
 from checkout.models import Order
@@ -30,6 +27,7 @@ def profile(request):
     }
 
     return render(request, template, context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
